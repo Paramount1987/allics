8064af535aec74b7183d534d3e28bbfb3669c09f
Artem Petrov <Paramount2009@yandex.ru>
2020-05-13 12:54:51 +0300

    adaptive nav

--- src/scripts/components/nav.js
+++ src/scripts/components/nav.js
@@ -1,16 +1,28 @@
 const Nav = {
     init() {
+        this.$body = $('body');
         this.$subList = $('.js-nav-sub-list');
+        this.$Nav = $('.js-nav');
+        this.$btnNav = $('.js-btn-nav');
+
         this.clickHandler();
         this.clickOutside();
+        this.btnClickHandler();
     },
 
     clickHandler() {
         $('.js-nav-link').click(function handler(e) {
             e.preventDefault();
-            const targetHref = $(this).attr('href');
-            const $target = $(targetHref);
-            Nav.$subList.fadeOut();
+            const currentId = $(this).attr('href');
+            const $target = $(currentId);
+
+            Nav.$subList.each(function eachNav() {
+                const id = $(this).attr('id');
+
+                if (currentId !== `#${id}`) {
+                    $(this).fadeOut();
+                }
+            });
             $target.fadeToggle();
         });
     },
@@ -22,6 +34,14 @@ const Nav = {
             if (!$target.hasClass('js-nav-link')) {
                 Nav.$subList.fadeOut();
             }
+        });
+    },
+
+    btnClickHandler() {
+        Nav.$btnNav.click(() => {
+            Nav.$body.toggleClass('nav-open');
+            Nav.$Nav.fadeToggle();
+            Nav.$btnNav.toggleClass('active');
         });
     },
 };
